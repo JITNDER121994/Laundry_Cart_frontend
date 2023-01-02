162cd66eb62102ba082539f9e6f0ff3b236c9c63
Harshitha P <65996712+puffy101@users.noreply.github.com>
2023-01-03 00:20:49 +0530

    Removed empty array

--- src/Home.jsx
+++ src/Home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
         setFetchedData(data.orders.reverse());
         console.log(data);
       });
-  }, []);
+  });
 
   const handleCreateClick = () => {
     navigate("/catelog");
